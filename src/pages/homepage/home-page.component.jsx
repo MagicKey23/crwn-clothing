f0e42f70a586e817d6f861a427e5db087705d8c6
Kaney Nguyen <kaneyz112@gmail.com>
2019-07-31 23:57:44 -0400

    Create new components and style scss. Finished menu items

---
+++ src/pages/homepage/home-page.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './home-page.styles.scss';
+import {Directory} from '../../component/directory/directory-component'
+  export const HomePage = () =>(
+    <div className='homepage'>Home Page
+    <Directory/>
+    </div>
+);
+
